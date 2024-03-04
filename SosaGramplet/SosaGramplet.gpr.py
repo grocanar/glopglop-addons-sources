a1af4fa316ff84f8470ca5f78e0cc2cbf51df6de
grocanar <eric@doutreleau.fr>
2024-03-04 21:46:08 +0100

    update

--- SosaGramplet/SosaGramplet.gpr.py
+++ SosaGramplet/SosaGramplet.gpr.py
@@ -4,7 +4,7 @@ register(GRAMPLET,
 	name=_("Sosa Gramplet"),
 	description = _("a gramplet that displays sosa of a person"),
 	status = STABLE, # not yet tested with python 3
-	version = '1.0.28',
+	version = '1.0.29',
 	fname="SosaGramplet.py",
 	height = 200,
 	gramplet = 'SosaGramplet',
